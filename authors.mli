bdedc236909039b141f1f534a14483177aa7d258
Natalie Isak <43297023+natalieisak@users.noreply.github.com>
2020-05-20 10:38:30 -0400

    Delete authors.mli

--- authors.mli
+++
@@ -1,8 +0,0 @@
-(**
-   Metadata about this submission.
-   @author Natalie Isak ngi2, Brett Schlesinger bgs59, Lillian Joyce lhj32. 
-*)
-
-(** [hours_worked] is a list of the number of hours each team member
-    worked on this assignment. *)
-val hours_worked : int list
